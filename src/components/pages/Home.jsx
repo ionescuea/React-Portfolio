2da7404a8befea6b32749ba013bebbbee07633bb
ionescuea <ionescuea@yahoo.com>
2024-03-04 18:21:01 +0000

    Add 'home' code; restructure css for the Navbar; add photo

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,12 +1,59 @@
-// import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Home = () => (
-  <div>
-    <h1>Home Page</h1>
-    <p>
-    Home Page
-    </p>
+  <div className="container">
+    <div className="row">
+      <div className="col-4">
+        <div className="h1 mt-5 mb-5 pb-5 pt-5">
+          Hello and welcome to my portfolio!
+        </div>
+        <div className="intro card mt-5 shadow-lg p-3 mb-5 rounded fw-semibold bg-light">
+          Hi, I&apos;m Alexandra. I&apos;m a front-end developer based in Plymouth, UK. I have
+          experience in HTML, CSS, JavaScript and React. I&apos;m passionate about creating
+          beautiful and performant websites. I&apos;m always looking for new challenges
+          and opportunities to learn. If you&apos;re interested in working with me, please
+          get in touch. I&apos;d love to hear from you!
+        </div>
+      </div>
+
+      <div className="col-4">
+        <img src="src/assets/alex-photo.jpg" alt="Alex's picture" className="rounded-5 mx-auto d-block img-thumbnail mt-3 p-0"></img>
+      </div>
+
+      <div className="col-4 flex">
+        <div className="col-3 card shadow rounded w-100 mt-5 mb-5">
+          <div className="card-body">
+            <div className="quotes fst-italic">
+              &quot;Great things are done by a series of small things brought together.&quot; Vincent Van Gogh
+            </div>
+          </div>
+        </div>
+        <div className="col-3 card shadow rounded w-100 mt-5 mb-5">
+          <div className="card-body">
+            <div className="quotes fst-italic">
+              &quot;So much depends on our attitude.&quot; Thomas S. Monson
+            </div>
+          </div>
+        </div>
+        <div className="col-3 card shadow rounded w-100 mt-5 mb-5">
+          <div className="card-body mx-auto">
+            <div className="quotes fst-italic">
+              &quot;Attitude is a little thing that makes a big difference.&quot; Winston Churchill
+            </div>
+          </div>
+        </div>
+        <div className="col-3 card shadow rounded w-100 mt-5 mb-5">
+          <div className="card-body mx-auto">
+            <div className="quotes fst-italic">
+              &quot;There are three ways to ultimate success: The first way is to be kind. The second way is to be kind. The third way is to be kind.&quot; Mister Rogers
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 );
+
+
 
 export default Home;
