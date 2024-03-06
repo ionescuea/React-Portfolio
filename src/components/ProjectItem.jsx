106c11c833654fbb54c09deb83d36640b284f153
ionescuea <ionescuea@yahoo.com>
2024-03-06 21:34:05 +0000

    Add resume to footer; restyle projects

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -3,7 +3,7 @@ import "../styles/index.css";
 
 function ProjectItem(props) {
   return (
-    <div className="pg-card d-flex flex-row mt-3" style={{ width: "20rem" }}>
+    <div className="pg-card d-flex flex-row pb-0" style={{ width: "20rem" }}>
       <div className="row">
         <p className="card-title h5 text-center">{props.title}</p>
         <p className="text-center">{props.tech}</p>
@@ -11,7 +11,7 @@ function ProjectItem(props) {
         <a href={props.project_url} className="card-link" target='_blank'>
           <img src={props.image_url} className="card-img-top m-0" alt={props.title} /></a>
         <div className="card-body p-0 col-sm-12 col-lg-6">
-          <div className="card-text">{props.description}</div>
+          <div className="card-text fst-italic text-left">{props.description}</div>
         </div>
       </div>
     </div>
--- src/components/pages/ProjectGallery.jsx
+++ src/components/pages/ProjectGallery.jsx
@@ -13,14 +13,14 @@ const ProjectGallery = () => (
       <hr></hr>
       <div className="row justify-content-around">
         <ProjectItem title="Weather Dashboard" image_url="src/assets/weather-dashboard.jpg" project_url="https://ionescuea.github.io/Weather-Dashboard/" tech="JavaScript & API's" description="This project displays the weather and the forecast for the next 5 days, using API&apos;s and local storage" />
-        <ProjectItem title="Work Day Scheduler" image_url="src/assets/work-day-scheduler.jpg" project_url="https://ionescuea.github.io/Work-Day-Scheduler/" tech="JavaScript & Moment.js" description="This project allows the user to set up a day and time to be scheduled" />
-        <ProjectItem title="Readme Generator" image_url="src/assets/readme-generator.jpg" project_url="https://github.com/ionescuea/Readme-Generator" tech="CLI Application, ES6, Node.js" description="This project generates a readme with multiple-choice questions" />
+        <ProjectItem title="Work Day Scheduler" image_url="src/assets/work-day-scheduler.jpg" project_url="https://ionescuea.github.io/Work-Day-Scheduler/" tech="JavaScript, jQuery & Daily.js" description="This project allows the user to set up a day and time to be scheduled" />
+        <ProjectItem title="Readme Generator" image_url="src/assets/readme-generator.jpg" project_url="https://github.com/ionescuea/Readme-Generator" tech="CLI Application, JavaScript, ES6, Node.js" description="This project generates a readme with multiple-choice questions" />
       </div>
 
       <div className="row justify-content-around">
         <ProjectItem title="Photo Music" image_url="src/assets/photo-music.jpg" project_url="https://mannyigho.github.io/photo-music/" tech="HTML, CSS Bootstrap, JavaScript, API's" description="This project allows the user to search for the latest photo and music" />
-        <ProjectItem title="Horiseon" image_url="src/assets/horiseon.jpg" project_url="https://ionescuea.github.io/Horiseon/" tech="HTML, CSS, JavaScript" description="This project displays Horiseon&apos;s social media presence" />
-        <ProjectItem title="Team Profile Generator" image_url="src/assets/my-team.jpg" project_url="https://github.com/ionescuea/Team-Profile-Generator" tech="CLI Application, ES6, Node.js" description="This application will take in information about employees, then generates an HTML webpage that displays summaries for each person." />
+        <ProjectItem title="Horiseon" image_url="src/assets/horiseon.jpg" project_url="https://ionescuea.github.io/Horiseon/" tech="HTML, CSS" description="This project displays Horiseon&apos;s social media presence" />
+        <ProjectItem title="Team Profile Generator" image_url="src/assets/my-team.jpg" project_url="https://github.com/ionescuea/Team-Profile-Generator" tech="CLI Application, ES6, Node.js, JavaScript" description="This application will take in information about employees, then generates an HTML webpage that displays summaries for each person." />
       </div>
     </div>
   </div>
