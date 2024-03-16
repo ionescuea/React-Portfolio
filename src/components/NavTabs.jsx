5489fa4102e57a577ff693b434ecb27c2e79a914
ionescuea <ionescuea@yahoo.com>
2024-03-16 13:28:32 +0000

    Update nav transition and active class

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -17,44 +17,47 @@ function NavTabs(props) {
         <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-      <div className="offcanvas offcanvas-end" tabIndex="-5" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-      <div className="offcanvas-header">
-        <h5 className="offcanvas-title" id="offcanvasNavbarLabel">AI ionescuEA</h5>
+        <div className="offcanvas offcanvas-end" tabIndex="-5" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+          <div className="offcanvas-header">
+            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">AI ionescuEA</h5>
+          </div>
+          <div className="offcanvas-body">
+            <ul className="nav flex-column">
+              <li className="nav-item">
+                <a
+                  href="#Home"
+                  onClick={() => handlePageChange('Home')}
+                  className={`nav-link ${props.currentPage === 'Home' ? 'active' : ''}`}
+                  data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
+                >
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  href="#Contact"
+                  onClick={() => handlePageChange('Contact')}
+                  className={`nav-link ${props.currentPage === 'Contact' ? 'active' : ''}`}
+                  data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
+                >
+                  Contact
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  href="#ProjectGallery"
+                  onClick={() => handlePageChange('Project Gallery')}
+                  className={`nav-link ${props.currentPage === 'Project Gallery' ? 'active' : ''}`}
+                  data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
+                >
+                  Project Gallery
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
-      <div className="offcanvas-body">
-        <ul className="nav flex-column">
-          <li className="nav-item">
-            <a
-              href="#Home"
-              onClick={() => handlePageChange('Home')}
-              className={`nav-link ${props.currentPage === 'Home' ? 'active' : ''}`}
-            >
-              Home
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              href="#Contact"
-              onClick={() => handlePageChange('Contact')}
-              className={`nav-link ${props.currentPage === 'Contact' ? 'active' : ''}`}
-            >
-              Contact
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              href="#ProjectGallery"
-              onClick={() => handlePageChange('Project Gallery')}
-              className={`nav-link ${props.currentPage === 'Project Gallery' ? 'active' : ''}`}
-            >
-              Project Gallery
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </div>
-</nav>
+    </nav>
   );
 }
 
