f2c74b18553c927380331623a23234408d86e0da
ionescuea <60351947+ionescuea@users.noreply.github.com>
2024-11-14 23:23:57 +0000

    Redesign home page; installed iconify; add new photo

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,78 +1,68 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { ImHtmlFive } from "react-icons/im";
-import { IoLogoCss3 } from "react-icons/io";
-import { RiBootstrapFill } from "react-icons/ri";
-import { RiJavascriptFill } from "react-icons/ri";
-import { BiLogoReact } from "react-icons/bi";
-import { TbBrandNodejs } from "react-icons/tb";
-import { DiJqueryUiLogo } from "react-icons/di";
-import { ImGithub } from "react-icons/im";
+import { Icon } from '@iconify/react';
+
 
 const Home = () => (
-  <div className="container">
-    <div className="row align-items-center justify-content-center h-100">
-      <div className="col-lg-4 col-sm-12">
-        <div className="h1 text-center">
-          Hello and welcome to my portfolio!
-        </div>
-        <div className="intro card mt-5 shadow-lg p-3 rounded fw-semibold bg-light">
-          Hi, my name is Alexandra. I specialize in front-end development, with expertise in HTML, CSS, JavaScript,
-          and React. My passion lies in crafting visually stunning and high-performance websites.
-          I actively seek out new challenges and opportunities for growth. If you are interested
-          in collaborating, please reach out. I am eager to connect with you!
-        </div>
-          <div className="h5 mt-3 pt-3 pb-1 text-center">
-            Frontend Technologies used
-          </div>
-          <div className="intro card shadow-lg rounded fw-semibold bg-light pt-2 pb-2">
-            <div className="d-flex justify-content-around align-items-center h-100 mt-3">
-              <p><ImHtmlFive />HTML</p>
-              <p><IoLogoCss3 />CSS</p>
-              <p><RiBootstrapFill />Bootstrap</p>
-              <p><RiJavascriptFill />JavaScript</p>
-            </div>
-            <div className="d-flex justify-content-around align-items-center h-100 mt-3">
-              <p><DiJqueryUiLogo />jQuery</p>
-              <p><TbBrandNodejs />Node.js</p>
-              <p><BiLogoReact />React</p>
-              <p><ImGithub />GitHub</p>
-            </div>
-          </div>
+  <div className="container-fluid py-3 my-3">
+    <div className="row">
+      <div className="col-lg-12 text-center">
+        <h1 className="greetings row align-items-center justify-content-center pt-md-10 pb-md-10 mb-md-10 w-100">Alexandra Ionescu</h1>
       </div>
-
-      <div className="col-lg-4 col-sm-12 mt-3">
-        <img src="alex-photo.jpg" alt="Alex's picture" className="img-fluid rounded-5 d-block img-thumbnail"></img>
+    </div>
+    <div className="row mt-5">
+      <div className="col-lg-7 col-sm-12 d-flex flex-column justify-content-center align-items-center py-3 my-3">
+        <div className="h2 text-center">
+          Welcome to my portfolio!
+        </div>
+        <div className="intro-card justify-content-center shadow rounded fw-semibold bg-light p-3 m-3">
+          Specializing in front-end development with skills in HTML, CSS, JavaScript, and React, my focus is on crafting clean, responsive websites that work seamlessly across all devices. Building user-friendly and visually appealing interfaces is a priority, with a dedication to code that's both efficient and maintainable.
+          Always open to learning new tools and approaches, especially in fast-evolving front-end development, I'm excited to take on projects that offer growth and challenge. Feel free to explore the projects featured here, and don't hesitate to reach out for potential collaborations!
+        </div>
+        <div className="h5 text-center py-3">
+          Frontend Technologies used
+        </div>
+        <div className="intro-card justify-content-center shadow rounded fw-semibold bg-light p-3 m-3">
+          <div className="d-flex text-center justify-content-around align-items-center pt-2 pb-2">
+            <div>
+              <Icon icon="logos:html-5" width="24" height="24" />
+              <span className="d-none d-sm-inline-block">HTML</span>
+            </div>
+            <div>
+              <Icon icon="logos:css-3" width="24" height="24" />
+              <span className="d-none d-sm-inline-block">CSS</span>
+            </div>
+            <div>
+              <Icon icon="logos:bootstrap" width="24" height="24" />
+              <span className="d-none d-sm-inline-block">Bootstrap</span>
+            </div>
+            <div>
+              <Icon icon="logos:javascript" width="24" height="24" />
+              <span className="d-none d-sm-inline-block">JavaScript</span>
+            </div>
+          </div>
+          <div className="d-flex text-center justify-content-around align-items-center pt-2 pb-2">
+            <div>
+              <Icon icon="logos:react" width="24" height="24" />
+              <span className="d-none d-sm-inline-block">React</span>
+            </div>
+            <div>
+              <Icon icon="logos:nodejs-icon" width="24" height="24" />
+              <span className="d-none d-sm-inline-block">Node.js</span>
+            </div>
+            <div>
+              <Icon icon="logos:github-icon" width="24" height="24" />
+              <span className="d-none d-sm-inline-block">GitHub</span>
+            </div>
+          </div>
+        </div>
       </div>
-
-      <div className="col-lg-4 col-sm-12 mt-5">
-        <div className="col-3 card shadow rounded w-100 mb-5">
-          <div className="card-body">
-            <div className="quotes fst-italic">
-              &quot;Great things are done by a series of small things brought together.&quot; Vincent Van Gogh
-            </div>
-          </div>
-        </div>
-        <div className="col-3 card shadow rounded w-100 mt-5 mb-5">
-          <div className="card-body">
-            <div className="quotes fst-italic">
-              &quot;So much depends on our attitude.&quot; Thomas S. Monson
-            </div>
-          </div>
-        </div>
-        <div className="col-3 card shadow rounded w-100 mt-5 mb-5">
-          <div className="card-body">
-            <div className="quotes fst-italic">
-              &quot;Attitude is a little thing that makes a big difference.&quot; Winston Churchill
-            </div>
-          </div>
-        </div>
-        <div className="col-3 card shadow rounded w-100 mt-5 mb-5">
-          <div className="card-body">
-            <div className="quotes fst-italic">
-              &quot;There are three ways to ultimate success: The first way is to be kind. The second way is to be kind. The third way is to be kind.&quot; Mister Rogers
-            </div>
-          </div>
-        </div>
+      <div className="photos col-lg-5 col-sm-12 d-flex flex-column justify-content-center py-3 my-3 p-3">
+        <img
+          src="portfolio-photos.jpg"
+          alt="Alex's pictures"
+          style={{ maxWidth: '400px' }}
+          className="img img-fluid rounded-5 d-block img-thumbnail mx-auto"
+        />
       </div>
     </div>
   </div>
