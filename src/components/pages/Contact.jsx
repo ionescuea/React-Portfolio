126a82bbe30119bd099fe46a3c1a48c392aae942
ionescuea <60351947+ionescuea@users.noreply.github.com>
2024-11-15 23:58:49 +0000

    Redesign contact page

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -4,30 +4,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function ContactFormModal() {
   const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [formData, setFormData] = useState({ email: '', message: '' });
-
-  // const handleChange = (e) => {
-  //   setFormData({ ...formData, [e.target.name]: e.target.value });
-  // };
-
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   // Send the email using the form data
-  //   window.location.href = `mailto:${formData.email}?subject=Portfolio Inquiry&body=${formData.message}`;
-  //   setModalIsOpen(false);
-  // };
 
   return (
-    <div className="container">
-      <div className="row align-items-center justify-content-center h-100">
-        <div className="col-12 col-md-6">
-          <div className="h1 mb-3 pb-3 pt-5 text-center">
+    <div className="container-fluid d-flex flex-column justify-content-center align-items-center" style={{ height: '80vh', paddingBottom: '10vh' }}>
+      <div className="row justify-content-center">
+        <div className="col-lg-12 col-sm-12">
+          <div className="h1 text-center" style= {{ fontFamily: 'Brush Script MT, cursive', fontSize: '4rem', fontWeight: 'bold', paddingBottom: '20px'}}>
             Get in touch
           </div>
-          <div className="intro card mt-5 mb-5 shadow-lg p-3 rounded fw-semibold bg-light text-center">
+          <div className="intro-modal-card shadow rounded fw-bold bg-light text-center my-5 py-3 px-5" style={{ marginTop: '40px' }}>
             What&apos;s holding you back? Send me a message!
-            <div className="d-flex justify-content-center align-items-center h-100 mt-3">
-              <button className="btn btn-primary" onClick={() => setModalIsOpen(true)}>Open Contact Form</button>
+            <div className="d-flex justify-content-center align-items-center py-3">
+              <button className="btn btn-primary col-sm-12 col-lg-6 mt-3" onClick={() => setModalIsOpen(true)}>Open Contact Form</button>
               <div className="row ">
                 <Modal
                   isOpen={modalIsOpen}
@@ -38,14 +26,8 @@ function ContactFormModal() {
                 >
                   <div className="container">
                     <div className="row justify-content-center">
-                      <div className="col-sm-6 col-md-6"></div>
-                      {/* <form className="p-3 rounded bg-light shadow w-100" onSubmit={handleSubmit}>
-                          <input type="email" name="email" value={formData.email} onChange={handleChange} className="form-control" placeholder="Your email" required />
-                          <textarea name="message" value={formData.message} onChange={handleChange} className="form-control mt-2" placeholder="Your message" required />
-                          <button type="submit" className="btn btn-primary mt-2">Send Email</button>
-                        </form> */}
                       <form className="p-3 rounded bg-light shadow w-100" name="contact" method="post">
-                      <input type="hidden" name="form-name" value="contact" />
+                        <input type="hidden" name="form-name" value="contact" />
                         <p>
                           <label htmlFor="email">Email </label>
                           <input type="email" name="email" id="email" required className="form-control" placeholder="Your email" />
