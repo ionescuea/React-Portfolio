09ffc76f08b539dd9e8a438acc4ada3ae9abac2d
ionescuea <60351947+ionescuea@users.noreply.github.com>
2026-03-25 13:20:55 +0000

    Restyle home page

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -13,7 +13,8 @@ function NavTabs(props) {
   return (
     <nav className="navbar bg-body-tertiary fixed-top">
       <div className="navTab container-fluid">
-        <a className="navbar-brand" href="#Home" onClick={() => handlePageChange('Home')}>ionescuEA</a>
+        <a className="navbar-brand" href="#Home" onClick={() => handlePageChange('Home')}>Alexandra Ionescu <br />
+          <p className="title">Front-End Developer | Digital & Technical Support</p></a>
         <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,25 +3,27 @@ import { Icon } from '@iconify/react';
 
 
 const Home = () => (
-  <div className="container-fluid py-3 my-3">
-    <div className="row">
+  <div className="container-fluid mt-0 pt-0 pb-3 mb-3">
+    {/* <div className="row">
       <div className="col-lg-12 text-center">
-        <h1 className="greetings row align-items-center justify-content-center pt-md-10 pb-md-10 mb-md-10 w-100">Alexandra Ionescu</h1>
+        <h1 className="greetings row align-items-center justify-content-center pt-md-10 pb-md-10 mb-md-10 w-100">I build responsive, user-friendly web applications using React, JavaScript, HTML, and CSS.
+        <br></br>With a background in business support and administration, I bring strong organisational skills, attention to detail, and a problem-solving mindset into every project.</h1>
       </div>
-    </div>
-    <div className="row mt-5">
-      <div className="col-lg-7 col-sm-12 d-flex flex-column justify-content-center align-items-center py-3 my-3">
+    </div> */}
+    <div className="row">
+      <div className="col-lg-7 col-sm-12 d-flex flex-column justify-content-center align-items-center pt-2 mt-2">
         <div className="h2 text-center">
           Welcome to my portfolio!
         </div>
-        <div className="intro-card justify-content-center shadow-lg rounded-5 fw-semibold bg-light p-3 m-3">
-          Specializing in front-end development with skills in HTML, CSS, JavaScript, and React, my focus is on crafting clean, responsive websites that work seamlessly across all devices. Building user-friendly and visually appealing interfaces is a priority, with a dedication to code that's both efficient and maintainable.
-          Always open to learning new tools and approaches, especially in fast-evolving front-end development, I'm excited to take on projects that offer growth and challenge. Feel free to explore the projects featured here, and don't hesitate to reach out for potential collaborations!
+        <div className="intro-card justify-content-center shadow-lg rounded-5 fw-semibold bg-light p-3 mb-3">
+            I am a front-end developer with a professional background in administration, project coordination, and digital systems. My experience working in structured, fast-paced environments has helped me develop strong organisational and communication skills, which I now apply to building efficient and user-focused web applications.
+            <br /><br />I have hands-on experience developing applications using React, JavaScript, HTML, and CSS, as well as working with APIs, WordPress, and website deployment.
+            <br /><br />I enjoy solving problems, improving user experience, and continuously learning new technologies. I am currently seeking opportunities where I can contribute to real-world projects while continuing to grow as a developer.
         </div>
-        <div className="h3 text-center py-3">
+        <div className="h3 text-center py-2">
           Frontend Technologies used
         </div>
-        <div className="intro-card justify-content-center shadow-lg rounded-5 fw-semibold bg-light p-3 m-3">
+        <div className="intro-card justify-content-center shadow-lg rounded-5 fw-semibold bg-light p-3 mb-4">
           <div className="d-flex text-center justify-content-around align-items-center pt-2 pb-2">
             <div>
               <Icon icon="logos:html-5" width="24" height="24" />
@@ -56,11 +58,11 @@ const Home = () => (
           </div>
         </div>
       </div>
-      <div className="photos col-lg-5 col-sm-12 d-flex flex-column justify-content-center py-3 my-3 p-3">
+      <div className="photos col-lg-5 col-sm-12 d-flex flex-column justify-content-center p-0 mb-1">
         <video
           src="video-design.mp4"
           alt="Alex's pictures"
-          style={{ maxWidth: '400px' }}
+          style={{ maxWidth: '350px' }}
           className="img img-fluid image-style animation-flipInY border-3 rounded-5 d-block img-thumbnail mx-auto shadow-lg"
           controls
           autoPlay
